4c60838b31c2610b505aed6990322eef7466e4d8
crparaizo <krisvran24@gmail.com>
2020-08-05 00:55:38 -0300

    ...

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -8,7 +8,7 @@ function CadastroCategoria() {
             <h1>Cadastro de Categoria</h1>
 
             <Link to="/">
-                Ir para Home k
+                Ir para Home
             </Link>
         </PageDefault>
     )
